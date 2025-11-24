a31896e52d6c79493210aa191b6bd304d99ccc09
NAMANPHOGAT <n4manphogat@gmail.com>
2025-11-24 13:32:08 +0530

    other.java

--- javaClass/other.java
+++ javaClass/other.java
@@ -5,7 +5,7 @@ public class other {
         System.out.println(s.Sum(10,20));
         protectedSub p = new protectedSub();
         System.out.println(p.Sub(60,23));
-        // privateProduct r = new privateProduct();
-        // System.out.println(r.Product(60,23));
+        
     }
 }
+
